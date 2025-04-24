bd380e3db1c9b50808df462d9841963ba7aa0be9
FAUSTO FERENTZ FILHO <fausto_ferentz-fi@estudante.sesisenai.org.br>
2025-04-24 08:38:10 -0300

    a base do codigo

--- prova.js
+++ prova.js
@@ -1,9 +1,16 @@
-class name {
-    constructor(parameters) {
-        
+
+class Personagem {
+    constructor(vida, forca, mana) {
+        this.vida = vida;
+        this.forca = forca;
+        this.mana = mana;
     }
-}   {
-    constructor(parameters) 
-        
-    
 }
+
+
+var personagem1 = new Personagem(100, 20, 50);
+
+
+console.log("Vida:", personagem1.vida);
+console.log("Força:", personagem1.forca);
+console.log("Mana:", personagem1.mana);
