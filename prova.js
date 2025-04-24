6a55fea6668795028f2f40f6aafefd9f7503effc
FAUSTO FERENTZ FILHO <fausto_ferentz-fi@estudante.sesisenai.org.br>
2025-04-24 08:47:28 -0300

    Criação das classes Guerreiro, Mago e Arqueiro herdando de Personagem

--- prova.js
+++ prova.js
@@ -1,4 +1,5 @@
 
+
 class Personagem {
     constructor(vida, forca, mana) {
         this.vida = vida;
@@ -8,9 +9,30 @@ class Personagem {
 }
 
 
-var personagem1 = new Personagem(100, 20, 50);
+class Guerreiro extends Personagem {
+    constructor(vida, forca) {
+        super(vida, forca, 0); 
+    }
+}
+
+class Mago extends Personagem {
+    constructor(vida, mana) {
+        super(vida, 5, mana); 
+    }
+}
+
+class Arqueiro extends Personagem {
+    constructor(vida, forca, mana) {
+        super(vida, forca, mana);
+    }
+}
 
 
-console.log("Vida:", personagem1.vida);
-console.log("Força:", personagem1.forca);
-console.log("Mana:", personagem1.mana);
+var guerreiro = new Guerreiro(150, 30);
+var mago = new Mago(100, 80);
+var arqueiro = new Arqueiro(120, 25, 40);
+
+
+console.log("Guerreiro - Vida:", guerreiro.vida, "Força:", guerreiro.forca, "Mana:", guerreiro.mana);
+console.log("Mago - Vida:", mago.vida, "Força:", mago.forca, "Mana:", mago.mana);
+console.log("Arqueiro - Vida:", arqueiro.vida, "Força:", arqueiro.forca, "Mana:", arqueiro.mana);
